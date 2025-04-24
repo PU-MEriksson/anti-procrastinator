77c588b0a7448777cc1bf03afd4d6dd88bf35004
PU-MEriksson <malin.eriksson83@icloud.com>
2025-04-24 14:06:42 +0200

    added heading prop with default value

--- client/src/components/Result.jsx
+++ client/src/components/Result.jsx
@@ -1,11 +1,11 @@
-function Result({ response }) {
+function Result({ response, heading = "Din handlingsplan" }) {
   return (
     <article
       className="result"
       aria-labelledby="result-heading"
       aria-live="polite"
     >
-      <h2 id="result-heading">Din personliga plan</h2>
+      <h2 id="result-heading">{heading}</h2>
       <p>{response}</p>
     </article>
   );
