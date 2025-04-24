df57190ea99a10d189d25bea7a79022713f52416
anna-dahlberg <anna.dahlberg@live.se>
2025-04-24 13:31:28 +0200

    added logic for react-router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,14 @@ import StartPage from './pages/StartPage';
 import ResultPage from './pages/ResultPage';
 
 function App() {
-
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+        <Route path="/result" element={<ResultPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
