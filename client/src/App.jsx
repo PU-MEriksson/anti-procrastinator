0344a5d983c707e4db044b1c279ae2a3872a0342
anna-dahlberg <anna.dahlberg@live.se>
2025-04-27 14:24:41 +0200

    added header and footer components to app and added styling for main container

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,38 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import styled from '@emotion/styled';
 import StartPage from './pages/StartPage';
 import ResultPage from './pages/ResultPage';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const MainContainer = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Content = styled.main`
+  flex: 1;
+  max-width: 800px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<StartPage />} />
-        <Route path="/result" element={<ResultPage />} />
-      </Routes>
+      <MainContainer>
+        <Header />
+        <Content>
+          <Routes>
+            <Route path="/" element={<StartPage />} />
+            <Route path="/result" element={<ResultPage />} />
+          </Routes>
+        </Content>
+        <Footer />
+      </MainContainer>
     </BrowserRouter>
   );
 }
