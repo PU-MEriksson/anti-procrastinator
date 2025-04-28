82bf872d39d4fdae1e7a13f1add193f753bd0774
PU-MEriksson <malin.eriksson83@icloud.com>
2025-04-28 14:42:20 +0200

    made changes to make vercel deployment work

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,12 @@ export default defineConfig({
     outDir: "dist",
     sourcemap: true,
   },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+  },
 });
